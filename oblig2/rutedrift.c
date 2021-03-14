b7e57164402bdf8f9c5f4ad282fbe727aff7dddd
Viktoria <tori.l.b.l.13@gmail.com>
2021-03-14 18:53:20 +0100

    oblig 2

--- oblig2/rutedrift.c
+++ oblig2/rutedrift.c
@@ -10,47 +10,64 @@ typedef struct infoBlokk
     int id;
     int flagg;
     char *modell; //produsent
-    struct infoBlokk *R2;
-    struct infoBlokk **pekere; //usikker her
+    struct infoBlokk **pekere;//usikker her
+    int antallPekere;
 } infoBlokk;
 
 
-
-enum type
-{
-    TRENGER, JEG, ENUM
-};
-
 //global liste over alle rutere
 struct infoBlokk **rutere = NULL;
+//globalt int om antall rutere
+int numberOfRutere = 0;
 
-
-void freeRute(infoBlokk *rute)
+void freeRute(int id)
 {
-    //free struct funksjon
+    free(rutere[findIndex(id)]);
 }
-
-
 
 void freeAllRuter()
 {
-    //free all structs funksjon
+    for (int i = 0; i < numberOfRutere;i++)
+    {
+        free(rutere[i]);
+        free(rutere[i]->modell);
+        free(rutere[i]->pekere);
+    }
+    free(rutere);
 }
 
+void printOneRute(int i)
+{
+    printf("ID: %d, Flagg: %d, Modell: %s\nKoblet med: \n", rutere[i]->id, rutere[i]->flagg, rutere[i]->modell);
+    for (int j = 0;j < rutere[i]->antallPekere; j++)
+    {
+        printf(stdout, "    ID: %d \n", rutere[i]->pekere[j]->id);
+    }
+    printf("\n");
 
+}
 
 void printAllInfo()
 {
-    //print info funksjoRuR
+    for (int i = 0; i <numberOfRutere; i++)
+    {
+        printf("ID: %d, Flagg: %d, Modell: %s\nKoblet med: \n", rutere[i]->id, rutere[i]->flagg, rutere[i]->modell);
+        for (int j = 0;j < rutere[i]->antallPekere; j++)
+        {
+                printf("    ID: %d \n", rutere[i]->pekere[j]->id);
+        }
+        printf("\n");
+    }
 }
-
-
 
 void createInfoBlokk(int id, int flagg, char *modell, int index)
 {
 
     infoBlokk *ruter = malloc(sizeof(struct infoBlokk));
-    rutere[index] = &ruter;
+    ruter->pekere  = malloc(sizeof(struct infoBlokk) * 10); 
+
+    rutere[index] = ruter; 
+    
     if (ruter == NULL)
     {
         fprintf(stderr, "mallloc failed, out of memory?\n");
@@ -59,31 +76,17 @@ void createInfoBlokk(int id, int flagg, char *modell, int index)
     }
 
     ruter->modell = strdup(modell); // les antall bytes og navn til char * i main
-    //sjekke feil?
-
+    ruter->antallPekere = 0;
     ruter->id = id; //gjør om fra bits til int i main
     ruter->flagg = flagg; // gjør om fra bits til int i main
-
+    //printf("id: %d, flagg : %d, produsent : %s\n", rutere[index]->id, rutere[index]->flagg, modell);
+    
 }
-
-
-
-
-
-//funksjon for å legge til koblinger
-    /* Gå gjennom alle rutere og lete etter R1 og R2? 
- 
-   Finnes det en bedre måte å gjøre det kanskje? */
-
-
-
-#define BUFFERSIZE 100
-#define EXPECTED_C 2 
 
 void makeSpaceForInfo(int N)
 {
     printf("%d\n", N);
-    rutere = malloc(sizeof(struct infoBlokk) * N);
+    rutere = malloc(sizeof(struct infoBlokk*) * N);
     //alloker nok plass til all structs 
     //og opprett et array med alle sammen som er globalt
 }
@@ -109,60 +112,133 @@ void readRuterFile(char *filen)
         exit(EXIT_FAILURE);
     }
 
-    int readCount;
+    int rc;
     unsigned char offset;
     int N;
 
-    readCount = fread(&offset, 1, 1, fil);
-    checkResult(readCount, fil);
-    N = (int)offset;
-    //allocer N plasser og opprett globalt array
+
+    rc = fread(&N, sizeof(int), 1, fil);
+    checkResult(rc, fil);
     makeSpaceForInfo(N);
+    numberOfRutere = N;
 
-    readCount = fseek(fil, (int)offset, SEEK_SET);
-    //while(fread(&offset, 1, 1, fil))     
-    //while(offset) //fseek? 
-    for (int i = 0; i < N;i++)
+    for (int i = 0; i < N; i++)
     {
-        unsigned char ruteID; //4 etterfølgene bytes
-        char flagg; //1 byte
-        char lengdeChar; //hvor lang produsent char * er, usikker på denne
-        char *produsent;
-        //lese alt i bytes og overføre til 
+        unsigned int ruteID; //4 etterfølgene bytes
+        unsigned char flagg; //1 byte
+        unsigned char lengdeChar; //hvor lang produsent char * er, usikker på denne
+        unsigned char produsent[249];
 
-        //DET ER NOE FEIL I INNLESNING
-        fread(&ruteID, sizeof(int), 1, fil);
-        checkResult(readCount, fil);
-        printf("%d ID\n", ruteID);
+        rc = fread(&ruteID, sizeof(int), 1, fil);
+        checkResult(rc, fil);
 
-        fread(&flagg, 1, 1, fil);
-        printf("%d flagg\n", flagg);
+        rc = fread(&flagg, sizeof(char), 1, fil);
+        checkResult(rc, fil);
 
-        fread(&lengdeChar, 1, 1, fil);
-        printf("%d lengdeChar\n", (int)lengdeChar);
-        
-        //fread(&produsent, 1, (int)lengdeChar, fil);
-        printf("produsent: %s\n", produsent);
+        rc = fread(&lengdeChar, sizeof(char), 1, fil);
+        checkResult(rc, fil);
+
+        rc = fread(&produsent, sizeof(char), sizeof(char) * lengdeChar, fil);
+        produsent[lengdeChar] = 0;
+
+        checkResult(rc, fil);
         createInfoBlokk(ruteID, flagg, produsent, i);
+
+        fseek(fil, 1, SEEK_CUR);
     }
 
-    if (ferror(fil))
+//LES KOBLINGER
+    unsigned int R1;
+    unsigned int R2;
+    while(fread(&R1, 4, 1, fil))
     {
-        fprintf(stderr, "fread failed\b");
+        fread(&R2, 4, 1, fil);
+
+        for (int i = 0; i < N; i++)
+        {
+            if (rutere[i]->id == R1) //Finner R1
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (rutere[j]->id == R2) // Finner R2
+                    {
+                        int plass = rutere[i]->antallPekere;
+                        rutere[i]->pekere[plass] = rutere[j];
+                        rutere[i]->antallPekere++;
+                        printf("id: %d, peker nå på id: %d\n", rutere[i]->id, rutere[j]->id );
+                        break;
+                        
+                    }
+                }
+            }
+        }
+    }
+    fclose(fil);
+}
+
+int findIndex(int id)
+{
+    for (int i = 0; i < numberOfRutere;i++)
+    {
+        if (rutere[i]->id == id)
+        {
+            return i;
+        }
+    }
+    return numberOfRutere+1;
+}
+
+void doCommand(char *command, int id, int id2, char *nyModell, char verdi, char nyFlagg)
+{
+    if (strstr(command, "print")){
+        printOneRute(findIndex(id));
+    }
+    if (strstr(command, "modell")){
+        rutere[findIndex(id)]->modell = nyModell;
+    }
+    if (strstr(command, "flagg")){
+        if (nyFlagg == '3') {printf("Ugyldig flaggverdi!\n");}
+        if ((int)nyFlagg > 8) {printf("Ugyldig flaggverdi!");}
+        else {
+            rutere[findIndex(id)]->flagg = nyFlagg+verdi;
+        }
+    }
+    if (strstr(command, "finnes")){
+        //DFS
+    }
+    if (strstr(command, "slett")){
+        //traversere?
+    }
+    if(strstr(command, "kobling")){
+        int n = rutere[findIndex(id)]->antallPekere;
+        rutere[findIndex(id)]->pekere[n+1] = rutere[findIndex(id2)];
+    } else {
+        printf("Det er ikke en kommando");
+    }
+
+}
+
+//funksjon som leser fil2 
+void readCommandsFile(char *filen)
+{
+    //FOPEN
+    FILE *fil = fopen(filen, "r");
+
+    if (fil == NULL)
+    {
+        perror("fopen");
         freeAllRuter();
         exit(EXIT_FAILURE);
     }
 
-    fclose(fil);
+    char *linje;
+    size_t lengde = 0;
+    //int readCount;
 }
 
 
-//funksjon som leser fil2 
-void readCommandsFile(char *fil)
-{
-
-}
-
+#define BUFFERSIZE 100
+#define EXPECTED_C 2 
 
 int main(int argc, char *argv[])
 {
@@ -176,9 +252,9 @@ int main(int argc, char *argv[])
     char *fil2 = argv[2];
     //process bits in file
     readRuterFile(fil1);
-    readCommandsFile(fil2);
+    //readCommandsFile(fil2);
+    printAllInfo();
     
-    
-    
+    freeAllRuter();
     return 0;
 }
